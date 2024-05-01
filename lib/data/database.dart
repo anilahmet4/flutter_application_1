cbb9017b48be9d19d9c1217148031ab7a9a20456
Mustafa Gümüştaş <mustafagumustas6@gmail.com>
2024-05-01 20:42:49 +0300

    deletion bug fixed

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -36,12 +36,32 @@ class HiveService {
   // Delete a receipt data based on key
   static Future<void> deleteReceipt(int key) async {
     var box = await _openBox();
-    await box.delete(key); // Deleting the receipt data at the specified key
+    await box.delete(key);
+    await reindexHiveBox(box);
   }
 
   // Clear all receipts from the box
   static Future<void> clearAllReceipts() async {
     var box = await _openBox();
     await box.clear(); // Clearing all data in the box
+  }
+
+  // Print all receipts with their keys
+  static Future<void> printAllReceipts() async {
+    var box = await _openBox();
+    var receipts = box.toMap().entries;
+    for (var entry in receipts) {
+      print('Key: ${entry.key}, Receipt: ${entry.value}');
+    }
+  }
+
+  static Future<void> reindexHiveBox(Box box) async {
+    var receipts = box.values.toList(); // Store all remaining receipts
+    await box.clear(); // Clear the box to reset indices
+
+    for (var receipt in receipts) {
+      await box.add(
+          receipt); // Reinsert each receipt; Hive will automatically assign new keys
+    }
   }
 }
